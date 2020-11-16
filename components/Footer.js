36d07f375fab6254644b787edd12b68d4870352d
Filipe Gomes <61765381+lipegomes@users.noreply.github.com>
2020-11-16 15:04:57 -0300

    New fix

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,16 @@
 import React from 'react'
 
-const Footer = () => {
+const Footer = ({ currentDate }) => {
   return (
     <div>
       <p className='text-center my-8 py-4 border-t-2'>
-        You can find the source-code of this website here:
+        This website was built during Fullstack Master classes. We used NextJS + SSR (Server Side Rendering) + Vercel (as platform)<br />
+        You can find the source-code of this website at:
         <br />
-        <a href=''>https://github.com/lipegomes/filipegomes.dev</a>
+        <a href='https://github.com/filipegomes/filipegomes.dev'>https://github.com/filipegomes/filipegomes.dev</a>
       </p>
+      <p className='text-center my-8 py-4 border-t-2'>Latest version: {currentDate}</p>
     </div>
   )
 }
-export default Footer
+export default  Footer
--- components/Hero/ContactMe.js
+++ components/Hero/ContactMe.js
@@ -6,10 +6,18 @@ const ContactMe = () => {
     <div className='mx-8 px-8 relative border-indigo500 border rounded md:mx-0 md:px-16 pb-4 pt-10 mt-6'>
       <h3 className='absolute bg-indigo500 py-2 px-6 top-0 -mt-5 text-2xl text-gray-100 font-bold uppercase'>Contact Me</h3>
       <p className='text-center md:text-left'>
-        <FaLinkedinIn className='text-teal-500 text-5xl md:text-6xl inline-block md:mr-6' />
-        <FaGithub className='text-teal-500 text-5xl md:text-6xl inline-block md:mr-6' />
-        <FaTelegram className='text-teal-500 text-5xl md:text-6xl inline-block md:mr-6' />
-        <FaLink className='text-teal-500 text-5xl md:text-6xl inline-block' />
+        <a href='https://www.linkedin.com/in/lipegomes/' title='Linkedin Profile'>
+          <FaLinkedinIn className='text-teal-500 text-5xl md:text-6xl inline-block md:mr-6' />
+        </a>
+        <a href='https://github.com/lipegomes' title='GitHub Profile'>
+          <FaGithub className='text-teal-500 text-5xl md:text-6xl inline-block md:mr-6' />
+        </a>
+        <a href='https://t.me/lord_fil' title='Telegram Contact'>
+          <FaTelegram className='text-teal-500 text-5xl md:text-6xl inline-block md:mr-6' />
+        </a>
+        <a href='https://blog.filipegomes.dev/' title='Filipe Gomes Blog'>
+          <FaLink className='text-teal-500 text-5xl md:text-6xl inline-block' />
+        </a>
         <br />
         <spam className='text-indigo-500 inline-block mt-4'>
           or drop a line: fgdl.py91@gmail.com
