7b5f68208bbe20a10e6db007dee27199c5241aaf
Filipe Gomes <61765381+lipegomes@users.noreply.github.com>
2020-11-18 11:47:07 -0300

    Fix all

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,22 @@
 import React from 'react'
-import PageHead from '../components/PageHead'
-import Hero from '../components/Hero'
-import Summary from '../components/Hero/Summary'
-import Education from '../components/Hero/Education'
-import Footer from '../components/Footer'
-import Repos from '../components/Repos'
 
-const Index = ({ repos, user }) => {
-  return (
-    <div className='container mx-auto bg-network'>
-      <PageHead />
-      <Hero />
-      <Summary />
-      <Education />
-      <Repos user={user} repos={repos} />
-      <Footer />
+const Index = (props) => {
+  return(
+    <div>
+      <h1>Bem vindo</h1>
+      <div>{props.currentDate}</div>
     </div>
   )
 }
-export async function getServerSideProps () {
-  const request = await fetch(process.env.API_URL + '/api/getUser')
-  const { repos, user } = await request.json()
+export async function getServerSideProps(context) {
+  const resRepos = fetch('https://api.github.com/users/lipegomes/repos?sort=updated')
+  const repos = await resRepos.json()
   return {
     props: {
       currentDate: new Date().toString(),
-      repos,
-      user
+      repos
     }
   }
 }
+
 export default Index
