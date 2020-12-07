068f0054231c7dd5a7d3268ce5a34d3ff18fd60b
Filipe Gomes <61765381+lipegomes@users.noreply.github.com>
2020-12-07 12:52:18 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-  purge: {
-    mode: 'all',
-    preserveHtmlElements: false,
-    content: [
-      './src/**/*.js',
-      './node_modules/flatpickr/**/*.js'
-    ]
-  },
   theme: {
     fontFamily: {
       sans: ['"Titillium Web"']
