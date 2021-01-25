56538349a445462ccf415c85379b79384fd76612
Filipe Gomes <fgdl.py91@gmail.com>
2021-01-25 13:10:06 -0300

    Update Index.js

--- components/Hero/Index.js
+++ components/Hero/Index.js
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className='md:grid md:grid-cols-2 leading-none'>
       <div className='pt-16 md:pt-32'>
         <h1 className='text-4xl uppercase pl-16'>Hi, I'm Filipe Gomes</h1>
-        <h2 className='text-4xl text-gray-100 font-bold md:text-5xl uppercase pl-16'>Junior Fullstack Developer</h2>
+        <h2 className='text-4xl text-gray-100 font-bold md:text-5xl uppercase pl-16'>Fullstack Engineer</h2>
         <CoontactMe />
       </div>
       {/* <div><img src='/images/filipegomes.png' /></div> */}
